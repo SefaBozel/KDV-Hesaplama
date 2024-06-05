b2c24bb31e3727486fed4574566d6c8a2377876c
SefaBozel <sefabozel57@gmail.com>
2024-06-05 21:51:42 +0300

    Add files via upload

---
+++ KDV hesaplama/Form6.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace arac
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(45, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(33, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ürün Fiyatı:";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(192, 255, 255);
+            textBox1.ForeColor = Color.FromArgb(0, 64, 0);
+            textBox1.Location = new Point(118, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(163, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(192, 255, 255);
+            textBox2.ForeColor = Color.FromArgb(0, 64, 0);
+            textBox2.Location = new Point(118, 133);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(163, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(192, 255, 255);
+            listBox1.ForeColor = Color.FromArgb(0, 64, 0);
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 226);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(492, 284);
+            listBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(192, 255, 255);
+            button1.ForeColor = Color.FromArgb(0, 64, 0);
+            button1.Location = new Point(355, 62);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 110);
+            button1.TabIndex = 5;
+            button1.Text = "HESAPLA";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 0);
+            ClientSize = new Size(526, 533);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form6";
+            Text = "Form6";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button1;
+    }
+}
---
+++ KDV hesaplama/Form6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arac
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urunadi;
+            int urunfiyat, kdv1, kdv2, kdv3, kdv4;
+            urunadi = textBox1.Text;
+            urunfiyat = Convert.ToInt16(textBox2.Text);
+            kdv1 = 8;
+            kdv2 = 18;
+            kdv1 = (urunfiyat+(urunfiyat * 8) / 100);
+            kdv2 = (urunfiyat+(urunfiyat* 18) / 100);
+            listBox1.Items.Add("%8 kdvli fiyat: "+kdv1+"\n"+" %18 kdvli fiyat: "+kdv2);
+        }
+    }
+}
